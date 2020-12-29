9c0106bd52a20559220e9942b09778c98d959a74
Ignacio Cervantes <i.cervantes@alumnos.upm.es>
2020-12-29 21:55:23 +0100

    Added actions, nlu data and modified stories

--- actions/actions.py
+++ actions/actions.py
@@ -21,9 +21,12 @@ def _get_server_client():
     server = jenkins.Jenkins('http://localhost:8080', username='admin', password='1234')
     return server
 
-def _launch_jenkins_job(job_name):
+def _launch_jenkins_job(job_name, params={}):
     jenkins_instance = _get_server_client()
-    jenkins_instance.build_job(job_name)
+    if params:
+        jenkins_instance.build_job(job_name, parameters=params)
+    else:
+        jenkins_instance.build_job(job_name)
 
 def _get_last_completed_job_info(jenkins_server, job_name):
     last_build_number = jenkins_server.get_job_info(job_name)['lastCompletedBuild']['number']
@@ -52,8 +55,38 @@ def _process_console_output(console_output):
         popped_line = console_lines.pop(0)
         if re.search('\+ sudo python', str(popped_line)) is not None:
             break
-    console_clean_output = '\n'.join(console_lines).replace('Finished: SUCCESS\n\n', '')
+    console_clean_output = '\n'.join(console_lines).replace('\n\nFinished: SUCCESS\n\n', '')
     return console_clean_output
+
+class action_inform_user(Action):
+    def name(self) -> Text:
+        return "action_inform_user"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        last_intent = tracker.latest_message['intent'].get('name')
+        if last_intent == 'server_state':
+            message = 'Checking server state...'
+        elif last_intent == 'launch_server':
+            message = 'Launching the server instance, this may take a while...'
+        elif last_intent == 'remove_server':
+            message = 'Removing the server instance, please stand by!'
+        elif last_intent == 'install_package':
+            message = 'Installing the requested package...'
+        elif last_intent == 'delete_package':
+            message = 'Delenting the requested package...'
+        elif last_intent == 'list_package':
+            message = 'Listing the installed packages'
+        elif last_intent == 'update_packages':
+            message = 'Updating the packages, this may take a while...'
+        elif last_intent == 'get_from_git':
+            message = 'Updating the packages from git, please stand by!'
+        dispatcher.utter_message(text=message)
+
+        return []
+
 
 class action_send_confirmation(Action):
 
@@ -134,10 +167,20 @@ class action_install_package(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        print(str(tracker.latest_message['entities']))
-        package_name = next(tracker.get_latest_entity_values('package'), None)
-        print('Accesed the action ' + self.name())
-        dispatcher.utter_message(text="Launched "+ self.name() + " and caught entity " + package_name)
+        parameters = {'PACKAGE': next(tracker.get_latest_entity_values('package'), None)}
+        if parameters['PACKAGE'] is not None:
+            dispatcher.utter_message(text="Launched "+ self.name())
+            jenkins_server = _get_server_client()
+            job_name = 'install_package'
+            print('Accesed the action ' + self.name() + ' with the entity ' + parameters['PACKAGE'])
+
+            current_job = _get_current_execution_number(jenkins_server, job_name)
+            _launch_jenkins_job(job_name, parameters)
+            job_console_results = _get_job_results(jenkins_server, job_name, current_job)
+            dispatcher.utter_message(text="Execution finished! Console output:\n" + job_console_results)
+        else:
+            dispatcher.utter_message("You tried to install a package? Sorry, I didn't got which one! Can you use the format \"package==1.0.0\" to specify the version?")
+
 
         return []
 
@@ -151,10 +194,19 @@ class action_delete_package(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        print(str(tracker.latest_message['entities']))
-        package_name = next(tracker.get_latest_entity_values('package'), None)
-        print('Accesed the action ' + self.name())
-        dispatcher.utter_message(text="Launched "+ self.name() + " and caught entity " + package_name)
+        parameters = {'PACKAGE': next(tracker.get_latest_entity_values('package'), None)}
+        if parameters['PACKAGE'] is not None:
+            dispatcher.utter_message(text="Launched "+ self.name())
+            jenkins_server = _get_server_client()
+            job_name = 'install_package'
+            print('Accesed the action ' + self.name() + ' with the entity ' + parameters['PACKAGE'])
+
+            current_job = _get_current_execution_number(jenkins_server, job_name)
+            _launch_jenkins_job(job_name, parameters)
+            job_console_results = _get_job_results(jenkins_server, job_name, current_job)
+            dispatcher.utter_message(text="Execution finished! Console output:\n" + job_console_results)
+        else:
+            dispatcher.utter_message("You tried to delete a package? Sorry, I didn't got which one! Can you use the format \"package==1.0.0\" to specify the version?")
 
         return []
 

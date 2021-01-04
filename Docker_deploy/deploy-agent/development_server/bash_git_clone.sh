cc1f1340ce662f108df51248679f6b4666e506f8
Ignacio Cervantes <i.cervantes@alumnos.upm.es>
2021-01-04 01:39:46 +0100

    Updated bash_git_clone.sh

--- Docker_deploy/deploy-agent/development_server/bash_git_clone.sh
+++ Docker_deploy/deploy-agent/development_server/bash_git_clone.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
+# Cleanup old files
 echo `rm -f requirements.txt`
+echo `rm -rf tfm-ignaciocervantes`
+# Clone the repo and checkout the requirements file
 echo `git clone -n http://icervillalon:FK8YN8QUkksGV6s2Sz69@lab.gsi.upm.es/TFM/tfm-ignaciocervantes.git --depth 1`
 echo `git --git-dir /home/tfm/prueba/tfm-ignaciocervantes/.git checkout HEAD development_requirements.txt`
 echo `cat development_requirements.txt`

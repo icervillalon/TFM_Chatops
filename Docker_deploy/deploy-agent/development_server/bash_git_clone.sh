6280186b1de8cad6cded036ae06e7d87f38c4a35
Ignacio Cervantes <i.cervantes@alumnos.upm.es>
2021-01-04 01:34:47 +0100

    Moved development_requirements.txt to root folder

--- Docker_deploy/deploy-agent/development_server/bash_git_clone.sh
+++ Docker_deploy/deploy-agent/development_server/bash_git_clone.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo `rm -f requirements.txt`
 echo `git clone -n http://icervillalon:FK8YN8QUkksGV6s2Sz69@lab.gsi.upm.es/TFM/tfm-ignaciocervantes.git --depth 1`
-echo `git --git-dir /home/tfm/prueba/tfm-ignaciocervantes/.git checkout HEAD requirements.txt`
-echo `cat requirements.txt`
+echo `git --git-dir /home/tfm/prueba/tfm-ignaciocervantes/.git checkout HEAD development_requirements.txt`
+echo `cat development_requirements.txt`
